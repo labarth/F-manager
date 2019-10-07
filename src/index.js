3716863065ea82ae8d66a85e2be9c22c920df338
alexander matiugin <alexander.matiugin@pandadoc.com>
2019-10-07 22:32:51 +0300

    base config webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader/root';
 
 const Component = () => <div>Hello World</div>;
+const ComponentWithHot = hot(Component);
 const rootEl = document.getElementById('root');
 
-ReactDOM.render(<Component />, rootEl);
+ReactDOM.render(<ComponentWithHot />, rootEl);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   devtool: 'source-map',
   mode: 'development',
-  entry: './src/index.js',
+  entry: ['react-hot-loader/patch', './src/index.js'],
   output:  {
     path: path.resolve(__dirname, 'build'),
     filename: 'build.js',
@@ -18,17 +18,9 @@ module.exports = {
   },
   devServer: {
     port: 2019,
-    compress: true, // enable gzip compression
-    historyApiFallback: true, // true for index.html upon 404, object for multiple paths
-    hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
-    https: false, // true for self-signed, object for cert authority
-    noInfo: true, // only errors & warns on hot reload
-    // ...
+    compress: true,
+    historyApiFallback: true,
+    hot: true,
   },
   plugins: [],
-  performance: {
-    hints: "warning", // enum
-    maxAssetSize: 200000, // int (in bytes),
-    maxEntrypointSize: 400000, // int (in bytes)
-  },
 };
