8eede221e0505c2596f720b88fa6e85271f358c1
alexander matiugin <alexander.matiugin@pandadoc.com>
2019-11-19 19:24:55 +0300

    update list service

--- server/index.js
+++ server/index.js
@@ -1,50 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
-const data = [
-  {
-    id: '1',
-    price: '400',
-    description: 'Lorem ipsum dolor sit amet.',
-  },
-  {
-    id: '2',
-    price: '550',
-    description: 'Lorem ipsum dolor sit amet.',
-  },
-  {
-    id: '3',
-    price: '430',
-    description: 'Lorem ipsum dolor sit amet.',
-  },
-  {
-    id: '4',
-    price: '500',
-    description: 'Lorem ipsum dolor sit amet.',
-  },
-  {
-    id: '5',
-    price: '450',
-    description: 'Lorem ipsum dolor sit amet.',
-  },
-];
+const data = JSON.parse(fs.readFileSync('notes.txt', 'utf-8'));
+
+const updateDataFile = (notes) => {
+  fs.writeFileSync('notes.txt', notes);
+};
 
 app.get('/api/items', (req, res) => {
   res.send(JSON.stringify(data));
 });
 
-app.post('/api/items', (req, res) => {
-  const item = req.body;
-  data.push(item);
-  res.json(data);
-});
-
 app.patch('/api/items', (req, res) => {
   const item = req.body;
   data.push(item);
+  updateDataFile(JSON.stringify(data));
   res.sendStatus(200);
 });
 
--- src/containers/List/service.js
+++ src/containers/List/service.js
@@ -4,4 +4,4 @@ const url = 'api/items';
 
 export const getNotes = () => CoreAPIService.get(url);
 
-export const addNote = (data) => CoreAPIService.patch(url, data.payload);
+export const addNote = (data) => CoreAPIService.patch(url, data);
