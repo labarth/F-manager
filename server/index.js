71008bd9a73d00538d3f38cb9978083c90c2c187
alexander matiugin <alexander.matiugin@pandadoc.com>
2020-01-09 16:18:26 +0300

    lint fix

--- server/index.js
+++ server/index.js
@@ -2,10 +2,57 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
+const http = require('http');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../build')));
+
+const server = http.createServer(app);
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (Number.isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+function onListening() {
+  var addr = server.address();
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 const data = JSON.parse(fs.readFileSync('notes.txt', 'utf-8'));
 
@@ -28,6 +75,6 @@ app.patch('/api/items', (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
