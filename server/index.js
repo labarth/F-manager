31f105a77fa13dc0ebaa6ce5cfabac77610ae962
alexander matiugin <alexander.matiugin@pandadoc.com>
2020-01-09 15:58:30 +0300

    lint fix

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../build')));
 
 const data = JSON.parse(fs.readFileSync('notes.txt', 'utf-8'));
 
@@ -13,6 +15,10 @@ const updateDataFile = (notes) => {
 
 app.get('/api/items', (req, res) => {
   res.send(JSON.stringify(data));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../', 'index.html'));
 });
 
 app.patch('/api/items', (req, res) => {
