aa827801d2fa702f44bf5ca611c302ae6f3b745d
alexander matiugin <alexander.matiugin@pandadoc.com>
2020-01-09 19:18:57 +0300

    lint fix

--- server/index.js
+++ server/index.js
@@ -8,51 +8,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../build')));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const server = http.createServer(app);
 
-const normalizePort = (val) => {
-  const port = parseInt(val, 10);
+const port = process.env.PORT || '3000';
 
-  if (Number.isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-};
-
-function onListening() {
-  var addr = server.address();
-}
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-
-const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+server.listen(port);
+
 
 const data = JSON.parse(fs.readFileSync('notes.txt', 'utf-8'));
 
@@ -64,17 +34,9 @@ app.get('/api/items', (req, res) => {
   res.send(JSON.stringify(data));
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'index.html'));
-});
-
 app.patch('/api/items', (req, res) => {
   const item = req.body;
   data.push(item);
   updateDataFile(JSON.stringify(data));
   res.sendStatus(200);
 });
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
